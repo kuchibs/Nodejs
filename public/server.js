926e13b16ab560591cd2d7354b0ea6649c65f39a
Sarvani kuhchibhotla <kuchibs@gmail.com>
2018-01-22 10:20:23 +0530

    refactor

---
+++ public/server.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+http.createServer(onRequest).listen(8890);
+console.log("Server is listening on port 8890");
+
+function onRequest(request, response) {
+    console.log("Server is in on Request: " + request.url);
+
+    response.writeHead(200, {"Context-Type": "type:plain/text"});
+    response.write("Hello World and Sarvani");
+    response.end();
+
+
+}
